908e9b495bff81a5ebc9fecca8520c05073bf9e4
Leonardo Vieira Neto <neto.vieira.leonardo@gmail.com>
2018-08-14 15:01:12 -0300

    Criando o projeto

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Projeto de estudos de React
+        </strong>
+    </footer>
+)
